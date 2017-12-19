59f7ff979e016f66c3d62c99028c51941658d4c3
Matthew Trueblood <E0202VA@ADSHQ.local>
2017-12-19 11:26:33 -0500

    Setup initial files

---
+++ index.js
@@ -0,0 +1,11 @@
+
+/* global __dirname */
+
+var express = require('express');
+var server = express();
+server.use(express.static(__dirname + "/public"));
+
+var port = 3000;
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
